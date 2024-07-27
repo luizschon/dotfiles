9016cb5e322eaaa129f944f187a91899f8104510
Luiz Schonarth <luizcschonarth@gmail.com>
2024-07-27 16:10:17 -0300

    Update wezterm fonts

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,7 +1,12 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.font = wezterm.font('Hack Nerd Font')
+config.font = wezterm.font_with_fallback({
+    'JetBrainsMono Nerd Font',
+    'Liberation Mono',
+    'Apple Color Emoji',
+    'Noto Color Emoji',
+})
 config.font_size = 12.0
 config.color_scheme = 'Ayu Dark (Gogh)'
 config.hide_tab_bar_if_only_one_tab = true
