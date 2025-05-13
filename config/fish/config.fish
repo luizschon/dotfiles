5960bfe4893e526f520271101ef06dfe9766c6ca
Luiz Schonarth <luizcschonarth@gmail.com>
2025-05-13 11:30:48 -0300

    Fix keychain initialization + some fish changes

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,30 +1,20 @@
 if status --is-login
     # Set environment variables
+    set -gx PNPM_HOME "/home/luiz/.local/share/pnpm"
+    if not string match -q -- $PNPM_HOME $PATH
+      set -gx PATH "$PNPM_HOME" $PATH
+    end
+
+    set -lx SHELL /usr/bin/fish
 end
 
 if status --is-interactive
     starship init fish | source
 
-    set -lx SHELL /usr/bin/fish
-
     set git_ssh_key github_tinyblk
     set gpg_key 5508FAC9F03AF83F
 
     if command -q keychain
-        keychain --quiet --eval $git_ssh_key $gpg_key | source
+        keychain --noinherit --eval $git_ssh_key $gpg_key | source
     end
-
-    if test "$TERM_PROGRAM" = "WezTerm"; and command -q fastfetch
-        fastfetch --gpu-hide-type integrated
-        echo ""
-    end
-
-    direnv hook fish | source
 end
-
-# pnpm
-set -gx PNPM_HOME "/home/luiz/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
